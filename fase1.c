4467cf2b03402ab469e3a2f2edc96d88c9a500fc
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-09-19 08:42:17 -0300

    Menor

--- fase1.c
+++ fase1.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 static long num_steps = 1000000;
+double step;
 
 void main () {
   int i; 
-  double x, pi, sum = 0.0;
-  const double step = 1.0/(double) num_steps;;
-  
-  #pragma omp parallel private(i, sum) shared(pi)
-  {
-    for (i = 0; i < num_steps; i++){
-      x = (i+0.5)*step;
-      sum = sum + 4.0/(1.0+x*x);
-    }
-    pi = step * sum;    
+  double x, pi;
+  step = 1.0/(double) num_steps;
+
+  #pragma omp parallel for reduction(+:pi)
+  for (i = 0; i < num_steps; i++){
+    x = (i+0.5)*step;
+    pi = pi + 4.0/(1.0+x*x);
   }
+  pi *= step;
   printf("Pi = %g\n", pi);
 }
